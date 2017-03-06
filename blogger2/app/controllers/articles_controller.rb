3b2a4a4d6c03a887cefa462703897c61fbfa55d1
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-06 21:57:17 +0100

    improve the layout

--- blogger2/app/controllers/articles_controller.rb
+++ blogger2/app/controllers/articles_controller.rb
@@ -1,9 +1,11 @@
 class ArticlesController < ApplicationController
 	def index
+		@title    = "All Articles"
 		@articles = Article.all
 	end
 
 	def show
 		@article = Article.find(params[:id])
+		@title   = @article.title
 	end
 end
