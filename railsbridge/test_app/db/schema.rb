c4bec1361f5157fc2383fa0b36aaa78bf998f09e
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-06 13:00:40 +0100

    heroku test app done

--- railsbridge/test_app/db/schema.rb
+++ railsbridge/test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306114141) do
+ActiveRecord::Schema.define(version: 20170306115341) do
 
   create_table "drinks", force: :cascade do |t|
     t.string   "name"
