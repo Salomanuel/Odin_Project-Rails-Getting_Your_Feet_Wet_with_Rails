2e65cb3f9a3c306dd4099a407363c61ee56e94ab
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-08 00:12:14 +0100

    add all tags index and add some links for navigation

--- blogger2/app/controllers/tags_controller.rb
+++ blogger2/app/controllers/tags_controller.rb
@@ -1,6 +1,11 @@
 class TagsController < ApplicationController
 	def show
-		@tag = Tag.find(params[:id])
+		@tag   = Tag.find(params[:id])
 		@title = "Articles tagged with \"#{@tag.name}\""
+	end
+
+	def index
+		@title = "Tags List"
+		@tags  = Tag.all
 	end
 end
