910ff8ae8a62ffb534aee901b8d19bc69480337a
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-07 14:28:37 +0100

    migrate db for comments

--- blogger2/db/migrate/20170307132610_create_comments.rb
+++ blogger2/db/migrate/20170307132610_create_comments.rb
@@ -2,7 +2,6 @@ class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
       t.string :author_name
-      t.string :string
       t.text :body
       t.references :article, foreign_key: true
 
